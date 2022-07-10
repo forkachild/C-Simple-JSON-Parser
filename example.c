5b16a20afca380dd8dc1ae980603097b2b187014
Suhel Chakraborty <forkachild@users.noreply.github.com>
2022-07-10 19:26:25 +0530

    Fixed empty object/array parsing issue (#8)
    * Fixed empty object/array parsing issue
    * Enhanced example

--- example.c
+++ example.c
@@ -1,12 +1,46 @@
-#include "example.h"
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "json.h"
 
+const char *read_file(const char *path) {
+    FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Expected file \"%s\" not found", path);
+        return NULL;
+    }
+    fseek(file, 0, SEEK_END);
+    long len = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char *buffer = malloc(len);
+
+    if (buffer == NULL) {
+        fprintf(stderr, "Unable to allocate memory for file");
+        fclose(file);
+        return NULL;
+    }
+
+    fread(buffer, 1, len, file);
+
+    return (const char *)buffer;
+}
+
 int main() {
+    const char *json = read_file("../asteroids.json");
+    if (json == NULL) {
+        return -1;
+    }
+
     json_object_t *object = json_parse(json);
+    free((void *)json);
+
+    if (object == NULL) {
+        return -1;
+    }
+
     json_print(object, 2);
     json_free(object);
+
     return 0;
 }
--- json.h
+++ json.h
@@ -13,3 +13,4 @@ declare_result_type(size);
 typed(json_object) * json_parse(typed(json_string) json_str);
 void json_print(typed(json_object) * obj, int indent);
 void json_free(typed(json_object) * obj);
+typed(json_string) json_error_to_string(const typed(json_error) err);
