a9bd36657c24c30af26c90d58ae4765909e16919
Suhel Chakraborty <chakraborty.suhel@gmail.com>
2022-07-18 22:54:19 +0530

    Hash table implementation done

--- example.c
+++ example.c
@@ -5,44 +5,47 @@
 #include "json.h"
 
 const char *read_file(const char *path) {
-    FILE *file = fopen(path, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Expected file \"%s\" not found", path);
-        return NULL;
-    }
-    fseek(file, 0, SEEK_END);
-    long len = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    char *buffer = malloc(len);
+  FILE *file = fopen(path, "r");
+  if (file == NULL) {
+    fprintf(stderr, "Expected file \"%s\" not found", path);
+    return NULL;
+  }
+  fseek(file, 0, SEEK_END);
+  long len = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  char *buffer = malloc(len + 1);
 
-    if (buffer == NULL) {
-        fprintf(stderr, "Unable to allocate memory for file");
-        fclose(file);
-        return NULL;
-    }
+  if (buffer == NULL) {
+    fprintf(stderr, "Unable to allocate memory for file");
+    fclose(file);
+    return NULL;
+  }
 
-    fread(buffer, 1, len, file);
+  fread(buffer, 1, len, file);
+  buffer[len] = '\0';
 
-    return (const char *)buffer;
+  return (const char *)buffer;
 }
 
 int main() {
-    const char *json = read_file("../reddit.json");
-    if (json == NULL) {
-        return -1;
-    }
+  const char *json = read_file("../sample/reddit.json");
+  if (json == NULL) {
+    return -1;
+  }
 
-    result(json_element) element_result = json_parse(json);
-    free((void *)json);
+  result(json_element) element_result = json_parse(json);
+  free((void *)json);
 
-    if (result_is_err(json_element)(&element_result)) {
-        typed(json_error) error = result_unwrap_err(json_element)(&element_result);
-        fprintf(stderr, "Error parsing JSON: %s\n", json_error_to_string(error));
-        return -1;
-    }
-    typed(json_element) element = result_unwrap(json_element)(&element_result);
-    json_print(&element, 2);
-    json_free(&element);
+  if (result_is_err(json_element)(&element_result)) {
+    typed(json_error) error = result_unwrap_err(json_element)(&element_result);
+    fprintf(stderr, "Error parsing JSON: %s\n", json_error_to_string(error));
+    return -1;
+  }
+  typed(json_element) element = result_unwrap(json_element)(&element_result);
+  typed(json_object) *obj = element.value.as_object;
 
-    return 0;
+  // json_print(&element, 2);
+  json_free(&element);
+
+  return 0;
 }
